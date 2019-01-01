69514bfb31de5fe1266e33263485121bf937689d
Miri.Hid <mirihdz11@gmail.com>
2019-01-01 14:42:09 -0500

    removed README

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The Punisher');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Princess', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Garbage Can', true);
